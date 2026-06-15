38f448696d8d3b385ad24e34f0178f26b7e8668b
FaniloMarinah <marinahfanilo@gmail.com>
2026-06-15 14:21:23 +0300

    Afficher liste de departement avec leur manager respective

---
+++ fonctions.php
@@ -0,0 +1,29 @@
+<?php
+function dbconnect(){
+    static $connect =null;
+    if ($connect===null) {
+        $connect=mysqli_connect('localhost','root','','employees');
+        if (!$connect) {
+            die('Erreur de connexion a la base de donnees : ' . mysqli_connect_error());
+        }
+        mysqli_set_charset($connect, 'utf8mb4');
+    }
+    return $connect;
+}
+
+function liste_departement(){
+    $sql="SELECT dept.dept_no,dept.dept_name,e.last_name,e.first_name 
+    FROM departments as dept JOIN dept_manager as dept_m ON 
+    dept.dept_no=dept_m.dept_no JOIN employees as e ON 
+    e.emp_no=dept_m.emp_no WHERE dept_m.to_date = '9999-01-01'";
+    $resultat=mysqli_query(dbconnect(),$sql);
+    $tableau=array();
+    while ($donne=mysqli_fetch_assoc($resultat)) {
+        $tableau[]=$donne;
+    }
+mysqli_free_result($resultat);
+return $tableau;
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include("fonctions.php");
+    $departments=liste_departement();
+    ?>
+    <table>
+        <tr>
+        <th>Numero</th>
+        <th>Nom departement</th>
+        <th>Nom manager</th>
+        <?php for ($i=0; $i < count($departments); $i++) { ?>
+            <tr>
+                <td><?php echo $departments[$i]['dept_no']; ?></td>
+                <td><?php echo $departments[$i]['dept_name']; ?></td>
+                <td><?php echo $departments[$i]['last_name'] . " " . $departments[$i]['first_name']; ?></td>
+            </tr>
+        <?php   }?>
+        </tr>
+    </table>
+</body>
+</html>
